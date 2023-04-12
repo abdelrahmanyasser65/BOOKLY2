8d326570fb4022de0a64bdd435a8d0a22b4d9982
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-04-12 16:25:20 +0200

    colors

---
+++ lib/core/resources/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class ColorManager {
+  static Color primary = HexColor("#100B20");
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+  static Color grey = Colors.grey;
+}

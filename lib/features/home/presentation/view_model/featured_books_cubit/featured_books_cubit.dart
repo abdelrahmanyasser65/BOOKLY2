0f6ca74424646fa39072c58cf9a79deb8f5d181d
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-04-14 22:16:32 +0200

    cubit1

--- lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,22 @@
-import 'package:bloc/bloc.dart';
-import 'package:bookly/features/home/data/models/BookModel.dart';
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
+import 'package:bookly/features/home/data/models/BookModel.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void>fetchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+     var result= await homeRepo.fetchFeaturedBooks();
+     result.fold((failure) {
+       emit(FeaturedBooksFailure(failure.errorMessage));
+    }, (books)
+    {
+      emit(FeaturedBooksSuccess(books));
+     });
+  }
 }

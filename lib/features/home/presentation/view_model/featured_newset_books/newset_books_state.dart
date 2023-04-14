dd64144343139dd3b45633e120c59d8628919aab
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-04-14 22:26:40 +0200

    newsetbooks

---
+++ lib/features/home/presentation/view_model/featured_newset_books/newset_books_state.dart
@@ -0,0 +1,21 @@
+part of 'newset_books_cubit.dart';
+
+abstract class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+  @override
+  List<Object> get props => [];
+}
+
+class NewsetBooksInitial extends NewsetBooksState {}
+class NewsetBooksLoading extends NewsetBooksState {}
+class NewsetBooksFailure extends NewsetBooksState {
+  final String errorMessage;
+
+  const NewsetBooksFailure(this.errorMessage);
+}
+class NewsetBooksSuccess extends NewsetBooksState {
+final List<BookModel>books;
+
+ const NewsetBooksSuccess(this.books);
+
+}
